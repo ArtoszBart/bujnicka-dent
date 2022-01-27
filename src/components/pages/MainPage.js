f588c7f79e2a1ad9b02e6377a04de12b021da8d2
Bartosz Art <bartosz.art@gmail.com>
2022-01-27 20:04:26 +0100

    Created basic main page

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,13 @@
+import '../../styles/main.scss';
+
+function MainPage() {
+	return (
+		<main className="main-page">
+			<div className="main-page-device">
+				<h1><i>Autorski gabinet stomatologiczny</i> oparty na rodzinnej tradycji lekarskiej.</h1>
+			</div>
+		</main>
+	);
+}
+
+export default MainPage;

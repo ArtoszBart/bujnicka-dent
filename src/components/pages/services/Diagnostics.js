5dd78aa538e531d2a8af3fefab070b60d306e9b3
Bartosz Art <bartosz.art@gmail.com>
2022-01-31 17:08:46 +0100

    Added diagnosctics page

---
+++ src/components/pages/services/Diagnostics.js
@@ -0,0 +1,19 @@
+import '../../../styles/main.scss';
+
+function Diagnostics() {
+
+	return (
+		<main className="page-wrapper" role="main">
+			<div className="banner" id="diagnostics">
+				<div className="banner-title">
+					<h1>Wizyta Diagnostyczna</h1>
+				</div>
+			</div>
+			<section className="page-content">
+
+			</section>
+		</main>
+	);
+}
+
+export default Diagnostics;

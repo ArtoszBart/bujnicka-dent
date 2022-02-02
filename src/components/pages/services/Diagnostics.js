a773714dd0819d773e82fcdae7f94316c733a2eb
Bartosz Art <bartosz.art@gmail.com>
2022-02-02 14:23:32 +0100

    Addec Diagnostics to offer page

--- src/components/pages/services/Diagnostics.js
+++ src/components/pages/services/Diagnostics.js
@@ -10,9 +10,33 @@ function Diagnostics() {
 				</div>
 			</div>
 			<section className="page-content">
-
+				<p>Przychodząc do gabinetu stomatologicznego, często tylko wtedy,gdy coś wydarzy się z Twoim zębem,
+					skupiasz się na rozwiązaniu tego jednego problemu, może być inaczej...
+					Proponuję Ci BEZPŁATNĄ WIZYTĘ DIAGNOSTYCZNĄ, na której dowiesz się, co dzieje się z Twoimi wszystkimi zębami.
+				</p>
+				<p>
+					Wykorzystując badanie kliniczne oraz diagnostykę rentgenowską zostaniesz dokładnie zbadany i otrzymasz w formie pisemnej:
+				</p>
+				<ul>
+					<li>Plan leczenia</li>
+					<li>Wstępną symulację finansową</li>
+					<li>1-3 projekty uzupełnienia braków zębowych</li>
+					<li>
+						Często można też polepszyć wygląd Twojego uśmiechu,
+						stosując komputerowe projektowanie uśmiechu i nowoczesne
+						rozwiązania estetyczne - na wizycie diagnostycznej otrzymasz informację
+					</li>
+					<li>
+						Coraz częściej też obserwuję konieczność poprawienia warunków zwarciowych w jamie ustnej,
+						które są pomijane, a mają bardzo duży wpływ na zdrowie ogólne, np.występowanie migren.
+					</li>
+				</ul>
+				<p>
+					Korzystając z takiej wizyty, zyskujesz wiedzę na temat swoich zębów oraz całego narządu żucia.
+					Możesz też łatwiej zaplanować swoje finanse.
+				</p>
 			</section>
-		</main>
+		</main >
 	);
 }
 

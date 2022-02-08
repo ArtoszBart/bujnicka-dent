287091b4f38206c01b3dccf78a064311797f6a2b
Bartosz Art <bartosz.art@gmail.com>
2022-02-08 11:03:14 +0100

    Warnings fixes

--- src/components/fragments/Footer.js
+++ src/components/fragments/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 	return (
 		<footer>
 			<p>&copy; 2022 <a className="text-link" href="/">Bujnicka-Dent.pl</a></p>
-			<p>Made by <a className="text-link" href="http://52.174.165.195/" target="_blank">Bartosz Art</a></p>
+			<p>Made by <a className="text-link" href="http://52.174.165.195/" target="_blank" rel="noreferrer">Bartosz Art</a></p>
 		</footer>
 	)
 }
--- src/components/fragments/forms/customHooks/useContactForm.js
+++ src/components/fragments/forms/customHooks/useContactForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import validate from './../validation/validateContactForm';
 
 const useContactForm = () => {

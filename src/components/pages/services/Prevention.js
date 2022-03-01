ad5564f6248cfdfd8e62b26fe2ac64386b21aa10
Bartosz Art <bartosz.art@gmail.com>
2022-03-01 23:30:47 +0100

    Added plain text to Prevention page and fixed offers gallery titles

--- src/components/pages/services/Prevention.js
+++ src/components/pages/services/Prevention.js
@@ -9,9 +9,45 @@ function Prevention() {
 					<h1>Profilaktyka</h1>
 				</div>
 			</div>
-			<section className="page-content">
+			<article className="page-content">
+				<p>
+					PROFILAKTYKA to podstawowy dział stomatologii zajmujący się zapobieganiem chorób jamy ustnej oraz uświadamianiem, jak ważna w życiu codziennym jest prawidłowa higiena.
+					W zakres profilaktyki wchodzą:
+					- indywidualny INSTRUKTAŻ HIGIENY jamy ustnej;
+					- profesjonalny zabieg czyszczenia, obejmujący SCALING - usuwanie kamienia oraz czyszczenie z osadu;
+					- FLUORYZACJA, potocznie zwana lakierowaniem;
+					- LAKOWANIE;
+					- zabieg ICON;
+					- wskazówki żywieniowe;
+					- wizyty kontrolne.
 
-			</section>
+					Pozostająca na zębach fizjologiczna płytka nazębna, tworząca się po każdym posiłku z resztek jedzenia i bakterii, występuje w postaci miękkiego, ściśle przylegającego, biało - żółtego nalotu. Po kilku dniach ulega mineralizacji solami mineralnymi ze śliny i tworzy KAMIEŃ NAZĘBNY. Kamień powstaje więc na skutek nieprawidłowego czyszczenia zębów i nie da się go usunąć przy użyciu przyrządów do podstawowej higieny jamy ustnej. Pozbyć się go można tylko w gabinecie stomatologicznym za pomocą scalera, działającego na zasadzie ultradżwięków.
+					PŁYTKA NAZĘBNA jest podstawowym czynnikiem wywołującym powstanie próchnicy, a kamień - chorób przyzębia, dlatego SCALING powinien być wykonywany zawsze wtedy, kiedy pojawi się kamień. Częstość wykonywania tego zabiegu zależy więc od osobniczych predyspozycji pacjenta do jego odkładania.
+					Często kamień mylony jest z OSADEM NAZĘBNYM, który jest przebarwieniem zębów powstającym na skutek spożywania, zawierających silne substancje barwiące, napojów i pokarmów, np. kawy, herbaty, potraw z curry itp. oraz palenia papierosów. Usuwa się go razem z kamieniem podczas scalingu. SCALING nie jest bolesny, trwa od 15 do 50 min., w zależności od ilości złogów i kończy się polerowaniem zębów specjalną pastą. Zabieg powinien być wykonywany od 2 do 4 razy w roku, w zależności od indywidualnych potrzeb pacjenta.
+					Po zabiegu może pojawić się NADWRAŻLIWOŚĆ ZĘBÓW trwająca do 3-4 dni. W celu jej zmniejszenia, zaleca się zabezpieczenie zębów fluorem, potocznie nazywane lakierowaniem.
+					LAKIEROWANIE jest to zabieg polegający na aplikacji preparatów fluoru bezpośrednio na powierzchnię zęba lub zastosowanie przeznaczonych do tego łyżek i żelu. Stosowany w gabinecie fluor jest w wielokrotnie wyższym stężeniu niż preparaty ogólnodostępne.
+					FLUORYZACJA jest jednocześnie zabiegiem zapobiegającym powstawaniu próchnicy. Fluor, oprócz zmniejszania nadwrażliwości zębów poprzez remineralizację szkliwa, powoduje wzrost odporności zębów na działanie próchnicotwórcze bakterii.
+
+					CELEM PROFILAKTYKI jest takie poprowadzenie pacjenta, aby również w domu samodzielnie stosował przekazane mu zalecenia instruktażu higieny jamy ustnej oraz używał indywidualnie dobranych dodatkowych środków do higieny, takich jak:
+					- nici dentystyczne;
+					- szczoteczki międzyzębowe;
+					- szczoteczki jednopęczkowe.
+
+					Codzienne stosowanie pasty z fluorem, koniecznych dodatkowych środków do higieny oraz regularne usuwanie kamienia i osadu, warunkuje ochronę przed rozwojem próchnicy i chorób przyzębia.
+
+					Zabieg profesjonalnej higieny jamy ustnej stał się NAJCZĘŚCIEJ WYKONYWANĄ PROCEDURĄ STOMATOLOGICZNĄ.
+
+					LAKOWANIE jest zabiegiem wykonywanym u DZIECI w pełni wykształconych zębach stałych, w których anatomiczny kształt bruzd i zagłębień uniemożliwia ich skuteczne oczyszczanie. Płytka bakteryjna zalegająca w takim miejscu powoduje duże ryzyko powstania próchnicy zęba.
+
+					Lakowanie polega na BLOKADZIE ZAGŁĘBIEŃ ANATOMICZNYCH specjalnym lakiem, który uszczelnia je mechanicznie oraz uwalnia fluor. Uszczelnione bruzdy przestają być potencjalnym miejscem rozwoju najczęstszej choroby zębów.
+					Warunkiem nieinwazyjnego przeprowadzenia zabiegu jest całkowity brak plam próchniczych i ubytków w danym zębie. W przypadku wystąpienia próchnicy konieczne jest najpierw jej usunięcie.
+					Jest to istotny zabieg w profilaktyce próchnicy, ponieważ zabezpiecza zęby przed powstawaniem ubytków i gwarantuje ich długie przetrwanie w jamie ustnej.
+
+					ZABIEG ICON ( nazwa zabiegu pochodzi od nazwy fabrycznej stosowanego preparatu)- polega na likwidacji BIAŁYCH PLAM na powierzchni szkliwa, które są ODWAPNIENIAMI pojawiającymi się po leczeniu ortodontycznym lub antybiotykoterapii. Mogą również występować jako wrodzone defekty szkliwa.
+
+					Przeprowadza się go stosując płynną żywicę, która wielokrotnie wtarta w powierzchnię szkliwa powoduje zrekonstruowanie uszkodzonej powierzchni zęba. Jest to bardzo delikatna metoda leczenia wczesnego stadium próchnicy oraz defektów szkliwa, pozwalająca na przywrócenie naturalnego koloru i wyglądu zęba.
+				</p>
+			</article>
 		</main>
 	);
 }

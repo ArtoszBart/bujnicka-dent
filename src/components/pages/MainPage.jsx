68e6f8bd05427c46aa8bb5593fca290b0cb8fd0f
Bartosz Art <bartosz.art@gmail.com>
2022-06-05 22:27:15 +0200

    Rearranged smooth scrolling from jQuery to react

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import ParallaxBanner from '../fragments/ParallaxBanner'
 
@@ -5,18 +6,28 @@ function MainPage() {
 	const device1st = 'Autorski gabinet stomatologiczny';
 	const device2nd = 'oparty na rodzinnej tradycji lekarskiej';
 
+	const articleRef = useRef();
+
+	const handleScroll = () => {
+		window.scrollTo({
+			top: articleRef.current.offsetTop,
+			left: 0,
+			behavior: "smooth",
+		});
+	};
+
 	return (
 		<main className="main-wrapper page-wrapper" role="main">
 			<ParallaxBanner id="main-page" text={device1st} text2={'\n' + device2nd}>
 				<Link className="button" to="/appointment">Zapisz się na wizytę</Link>
-				<a className="scroll-down" href="#section1"><i className="fas fa-chevron-down"></i></a>
+				<Link className="scroll-down" to="/" onClick={handleScroll}><i className="fas fa-chevron-down"></i></Link>
 				<div className="quote">
 					<q>Salus aegroti suprema lex</q>
 					<p>Dobro chorego najwyższym prawem</p>
 					<p className="author">~Hipokrates</p>
 				</div>
 			</ParallaxBanner>
-			<article className="section" id="section1">
+			<article className="section" id="section1" ref={articleRef}>
 
 			</article>
 		</main>

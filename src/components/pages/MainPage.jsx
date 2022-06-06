74c1370dc8de67e2afadbd6f755019fc719e545b
Bartosz Art <bartosz.art@gmail.com>
2022-06-06 02:49:38 +0200

    Created main page content template

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import ParallaxBanner from '../fragments/ParallaxBanner'
+import ParallaxBanner from '../fragments/ParallaxBanner';
+import ImgLoading from '../fragments/ImgLoading';
+import DiagnosticsImg from '../../img/texts/diagnostics/plan.jpeg';
 
 function MainPage() {
 	const device1st = 'Autorski gabinet stomatologiczny';
@@ -20,15 +22,53 @@ function MainPage() {
 		<main className="main-wrapper page-wrapper" role="main">
 			<ParallaxBanner id="main-page" text={device1st} text2={'\n' + device2nd}>
 				<Link className="button" to="/appointment">Zapisz się na wizytę</Link>
-				<Link className="scroll-down" to="/" onClick={handleScroll}><i className="fas fa-chevron-down"></i></Link>
+				<Link className="scroll-down-button" to="/" onClick={handleScroll}><i className="fas fa-chevron-down"></i></Link>
 				<div className="quote">
 					<q>Salus aegroti suprema lex</q>
 					<p>Dobro chorego najwyższym prawem</p>
 					<p className="author">~Hipokrates</p>
 				</div>
 			</ParallaxBanner>
-			<article className="section" id="section1" ref={articleRef}>
-
+			<article ref={articleRef}>
+				<section aria-labelledby="diagnostic">
+					<div className='main-page-img'>
+						<ImgLoading src={DiagnosticsImg} alt="" />
+					</div>
+					<div className="main-page-text">
+						<h2 id="diagnostic">Bezpłatna wizyta diagnostyczna!</h2>
+						<p>
+							<b>Stomatologia zachowawcza</b> zajmuje się wykrywaniem i leczeniem najbardziej rozpowszechnionej choroby zębów,
+							czyli <b>próchnicy</b> wraz z odtwarzaniem utraconych tkanek wskutek tej choroby oraz urazów.
+						</p>
+						<Link className="button" to="/offer/diagnostics">Zobacz więcej</Link>
+					</div>
+				</section>
+				<section aria-labelledby="diagnostic">
+					<div className="main-page-text">
+						<h2 id="diagnostic">Bezpłatna wizyta diagnostyczna!</h2>
+						<p>
+							<b>Stomatologia zachowawcza</b> zajmuje się wykrywaniem i leczeniem najbardziej rozpowszechnionej choroby zębów,
+							czyli <b>próchnicy</b> wraz z odtwarzaniem utraconych tkanek wskutek tej choroby oraz urazów.
+						</p>
+						<Link className="button" to="/offer/diagnostics">Zobacz więcej</Link>
+					</div>
+					<div className="main-page-img">
+						<ImgLoading src={DiagnosticsImg} alt="" />
+					</div>
+				</section>
+				<section aria-labelledby="diagnostic">
+					<div className='main-page-img'>
+						<ImgLoading src={DiagnosticsImg} alt="" />
+					</div>
+					<div className="main-page-text">
+						<h2 id="diagnostic">Bezpłatna wizyta diagnostyczna!</h2>
+						<p>
+							<b>Stomatologia zachowawcza</b> zajmuje się wykrywaniem i leczeniem najbardziej rozpowszechnionej choroby zębów,
+							czyli <b>próchnicy</b> wraz z odtwarzaniem utraconych tkanek wskutek tej choroby oraz urazów.
+						</p>
+						<Link className="button" to="/offer/diagnostics">Zobacz więcej</Link>
+					</div>
+				</section>
 			</article>
 		</main>
 	);

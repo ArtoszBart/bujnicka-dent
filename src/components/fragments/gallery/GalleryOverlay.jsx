0fd856337cda2e68a18be5184a022edf3c6d817c
Bart <bart@MacBook-Pro-Bart.local>
2022-06-14 19:47:47 +0200

    Finished privacy policy page

--- src/components/fragments/gallery/GalleryOverlay.jsx
+++ src/components/fragments/gallery/GalleryOverlay.jsx
@@ -2,30 +2,32 @@ import ImgLoading from '../ImgLoading';
 
 function GalleryOverlay({ hook, multipleImages }) {
 	return (
-		<div className={`gallery-overlay${hook.isMounted ? ' opened' : ' closing'}`}
+		<div
+			className={`gallery-overlay${hook.isMounted ? ' opened' : ' closing'}`}
 			onAnimationEnd={hook.onAnimationEnd}
-			id="overlay-closing"
-			onClick={hook.closeGalleryOverlay}
-		>
-			<div className="top-pane" id="overlay-closing" onClick={hook.closeGalleryOverlay}>
-				<i className="fas fa-times" id="overlay-closing" />
+			id='overlay-closing'
+			onClick={hook.closeGalleryOverlay}>
+			<div className='top-pane' id='overlay-closing' onClick={hook.closeGalleryOverlay}>
+				<i className='fas fa-times' id='overlay-closing' />
 			</div>
-			{multipleImages &&
+			{multipleImages && (
 				<>
-					<div className="left-pane" onClick={hook.prevImage}>
-						<i className="fas fa-chevron-left" />
+					<div className='left-pane' onClick={hook.prevImage}>
+						<i className='fas fa-chevron-left' />
 					</div>
-					<div className="right-pane" onClick={hook.nextImage}>
-						<i className="fas fa-chevron-right" />
+					<div className='right-pane' onClick={hook.nextImage}>
+						<i className='fas fa-chevron-right' />
 					</div>
-					<div className="bottom-pane">
-						<p>{hook.openedImg + 1} / {hook.activeImageSet.length}</p>
+					<div className='bottom-pane'>
+						<p>
+							{hook.openedImg + 1} / {hook.activeImageSet.length}
+						</p>
 					</div>
 				</>
-			}
+			)}
 
-			<div className="img-box" id="overlay-closing">
-				<ImgLoading src={hook.activeImageSet[hook.openedImg]} alt="Certificate" />
+			<div className='img-box' id='overlay-closing'>
+				<ImgLoading src={hook.activeImageSet[hook.openedImg]} alt='Certificate' />
 			</div>
 		</div>
 	);
--- src/components/pages/PrivacyPolicy.jsx
+++ src/components/pages/PrivacyPolicy.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
+import React from 'react';
 import ParallaxBaner from '../fragments/ParallaxBanner';
 
 const PrivacyPolicy = () => {
 	return (
-		<main className="page-wrapper" role="main">
-			<ParallaxBaner id="privacy-policy" text="Polityka Prywatności" />
-			<article className="privacy-policy">
+		<main className='page-wrapper' role='main'>
+			<ParallaxBaner id='privacy-policy' text='Polityka Prywatności' />
+			<article className='privacy-policy'>
 				<section>
-					<h2><abbr title="Rozporządzenia o Ochronie Danych Osobowych">RODO</abbr></h2>
-					<p>
-						Przekazane przez Państwa dane osobowe będą przetwarzane przez:
-					</p>
+					<h2>
+						<abbr title='Rozporządzenia o Ochronie Danych Osobowych'>RODO</abbr>
+					</h2>
+					<p>Przekazane przez Państwa dane osobowe będą przetwarzane przez:</p>
 					<address>
 						Gabinet Stomatologiczny Magdalena Bujnicka <br />
 						ul.Andersena 6<br />
-						01-911 Warszawa<br />
+						01-911 Warszawa
+						<br />
 						NIP 118 016 37 66
 					</address>
+					<p>w celach związanych z udzielaniem usług medycznych.</p>
 					<p>
-						w celach związanych z udzielaniem usług medycznych.
-					</p>
-					<p>
-
 						Przysługuje Państwu prawo żądania od administratora danych dostępu do swoich danych, ich sprostowania, usunięcia lub
-						ograniczenia przetwarzania, a także prawo wniesienia skargi do organu nadzorczego. Odbiorcami danych mogą być podmioty zajmujące się
-						obsługą informatyczną lub marketingową administratora danych. Więcej informacji w dokumencie
-						"Ochrona danych osobowych" dostępnym na miejscu.
+						ograniczenia przetwarzania, a także prawo wniesienia skargi do organu nadzorczego. Odbiorcami danych mogą być podmioty
+						zajmujące się obsługą informatyczną lub marketingową administratora danych. Więcej informacji w dokumencie "Ochrona
+						danych osobowych" dostępnym na miejscu.
 					</p>
 				</section>
 				<section>
@@ -34,11 +32,10 @@ const PrivacyPolicy = () => {
 					<h3>Formularz kontaktowy</h3>
 					<ul>
 						<li>
-							<b>Imię i nazwisko</b> - abyśmy mogli skojarzyć z kim piszemy jeśli znamy nadawcę wiadomości,
-							oraz aby móc rozpoznać użycie zwrotu Pan/Pani
+							<b>Imię i nazwisko</b> -potrzebne do skojarzenia nadawcy, oraz prawidłowo użyć zwrotu "Pan/Pani"
 						</li>
 						<li>
-							<b>Email</b> - potrzebny do udzielenia odpowiedzi
+							<b>Email</b> - potrzebny do wysłania wiadomości zwrotnej
 						</li>
 					</ul>
 
@@ -63,13 +60,13 @@ const PrivacyPolicy = () => {
 
 					<h3>Zapis na wizytę</h3>
 					<p>
-						Dane pacjenta zapisanego na wizytę pozostają w naszej bazie danych do końca dnia, w którym ma odbyć się wizyta.
-						Pod koniec tego dnia, dane zostają usunięte z bazy danych.
+						Dane pacjenta zapisanego na wizytę pozostają w naszej bazie danych do końca dnia, w którym ma odbyć się wizyta. Pod
+						koniec tego dnia, dane zostają usunięte z bazy danych.
 					</p>
 				</section>
 			</article>
 		</main>
-	)
-}
+	);
+};
 
-export default PrivacyPolicy
+export default PrivacyPolicy;

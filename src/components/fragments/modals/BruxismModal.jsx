f6118f450bf52439de84075ee49ab6b916412b8f
Bart <bart@MacBook-Pro-Bart.local>
2022-06-15 15:43:50 +0200

    Added modals media querry

--- src/components/fragments/modals/BruxismModal.jsx
+++ src/components/fragments/modals/BruxismModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from '../Modal';
+import { Link } from 'react-router-dom';
 
 const BruxismModal = (props) => {
 	return (
@@ -7,23 +8,21 @@ const BruxismModal = (props) => {
 			{props.hook.openedModal === 'Bruksizm' && (
 				<Modal title='Bruksizm' hook={props.hook}>
 					<p>
-						Nieprawidłowa aktywność (parafunkcjonalna) narządu żucia, czyli występująca oprócz żucia, mowy i połykania. Odbywa się
-						przeważnie na poziomie nieuświadomionym w postaci pojedyńczego epizodu zaciśnięcia i zgrzytnięcia zębów lub
-						rytmicznych skurczów mięśni żucia prowadzących do serii zgrzytnięć.
+						Nieprawidłowa aktywność narządu żucia. Odbywa się przeważnie na poziomie nieuświadomionym w postaci zaciskania lub
+						zgrzytania zębami.
+					</p>
+					<p>Występuje zarówno w ciągu dnia jak i w czasie snu.</p>
+					<p>
+						Przyczyną wzmożonej aktywności parafunkcjonalnej mogą być przedwczesne kontakty zwarciowe, powodujące nadmierną
+						aktywność mięśniową. Są one odczuwane jako jako przeszkody na drodze zębów, a typową odpowiedzią organizmu jest
+						uaktywnienie układu mięśniowego w celu pozbycia się czynnika zaburzającego.
 					</p>
 					<p>
-						Występuje zarówno w ciągu dnia jak i w czasie snu. Ma podłoże psychologiczne wynikające z napięć emocjonalnych i
-						sytuacji stresowych.
-					</p>
-					<p>
-						Bruksizm manifestuje się obecnością na zębach startych powierzchni w postaci niewielkich, płaskich, lśniących
-						fragmentów nieodpowiadających ich morfologii. Przyczyną wzmożonej aktywności parafunkcjonalnej mogą być przedwczesne
-						kontakty zwarciowe (kontakty zębów górnych z dolnymi), powodujące nadmierną aktywność mięśniową. Są one odczuwane jako
-						jako przeszkody zaburzające równowagę nerwowo-mięśniową, a typową odpowiedzią organizmu jest uaktywnienie układu
-						mięśniowego w celu pozbycia się czynnika zaburzającego. Przyczynami powodującymi szukanie pomocy specjalistycznej są:
-						- niewytłumaczalny ból zębów lub mięśni - zmęczenie mięśni - znaczne pogorszenie estetyki uzębienia związane z jego
-						starciem. Do leczenia bruksizmu wykorzystuje się szyny relaksacyjne ( patrz LECZENIE STAWÓW SKRONIOWO-ŻUCHWOWYCH).
-						Może również wystąpić konieczność leczenia farmakologicznego przy zaburzeniach psychologicznych.
+						Do leczenia bruksizmu wykorzystuje się{' '}
+						<Link className='text-link' to={'/offer/joints'}>
+							szyny relaksacyjne
+						</Link>
+						.
 					</p>
 				</Modal>
 			)}

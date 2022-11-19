0b6de9ba7c584a3337480337b8b748b1e8074884
Bartosz Art <bartosz.art@gmail.com>
2022-11-19 19:26:22 +0100

    Removed contact form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import ContactForm from '../fragments/forms/ContactForm';
+// import { Link } from 'react-router-dom';
+// import ContactForm from '../fragments/forms/ContactForm';
 import ParallaxBaner from '../fragments/ParallaxBanner';
 
 function Contact() {
@@ -36,10 +36,10 @@ function Contact() {
 							Zapisz się na wizytę
 						</Link> */}
 					</div>
-					<ContactForm />
+					{/* <ContactForm /> */}
 				</section>
 
-				<section className='map-container' id='map'>
+				{/* <section className='map-container' id='map'>
 					<h3>Jak do nas trafić</h3>
 					<iframe
 						className='map'
@@ -47,7 +47,7 @@ function Contact() {
 						src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19526.233593659086!2d20.931493567993382!3d52.283710439202004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471eca33cc1b06f3%3A0xa69120edf8e6db80!2sHansa%20Christiana%20Andersena%206%2C%2001-911%20Warszawa!5e0!3m2!1spl!2spl!4v1644072280081!5m2!1spl!2spl'
 						loading='lazy'
 					></iframe>
-				</section>
+				</section> */}
 			</article>
 		</main>
 	);
